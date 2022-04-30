8f183c4084928e330edba1538c91143c36a06c83
blair65541468 <228842439@qq.com>
2022-04-30 11:30:02 +0800

    Add for git change

--- src/main/java/com/citi/test.java
+++ src/main/java/com/citi/test.java
@@ -2,11 +2,7 @@ package com.citi;
 
 public class test {
 
-
-
-
-    public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
 }
-
