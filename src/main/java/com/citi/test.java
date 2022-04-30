2d188b40c17fcc3bee6537b33ba64d96b6139913
blair65541468 <228842439@qq.com>
2022-04-30 15:59:58 +0800

    This is for combination test.
    Add for git change test 01.
    
    Add for git change test 02.
    
    Add for git change test 03.

--- src/main/java/com/citi/test.java
+++ src/main/java/com/citi/test.java
@@ -4,5 +4,8 @@ public class test {
 
   public static void main(String[] args) {
     System.out.println("Hello world");
+    System.out.println("Add for git test 01");
+    System.out.println("Add for git test 02");
+    System.out.println("Add for git test 03");
   }
 }
