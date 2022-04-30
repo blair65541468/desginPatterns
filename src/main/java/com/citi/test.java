f501545843f55c9203548e582990f8c3aecd8430
blair65541468 <228842439@qq.com>
2022-04-30 16:28:39 +0800

    Add for read me 01.

--- src/main/java/com/citi/test.java
+++ src/main/java/com/citi/test.java
@@ -4,8 +4,6 @@ public class test {
 
   public static void main(String[] args) {
     System.out.println("Hello world");
-    System.out.println("Add for git test 01");
-    System.out.println("Add for git test 02");
-    System.out.println("Add for git test 03");
+    System.out.println("Add for read me test01");
   }
 }
