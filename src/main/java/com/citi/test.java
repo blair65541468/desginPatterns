3005ecbb90941f3c13c91f1742a43336f19ebd8c
blair65541468 <228842439@qq.com>
2022-05-04 10:54:34 +0800

    Add for some log.

--- src/main/java/com/citi/test.java
+++ src/main/java/com/citi/test.java
@@ -5,5 +5,8 @@ public class test {
   public static void main(String[] args) {
     System.out.println("Hello world");
     System.out.println("Add for read me test01");
+
+    System.out.println("Test for Stash ----before");
+
   }
 }
